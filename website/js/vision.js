9ae51671e2547a4f2f43b5078dfc90f8404cb702
Valentin Quelquejay <valentin.quelquejay-leclere@epfl.ch>
2020-04-21 09:56:19 +0200

    first vision viz

---
+++ website/js/vision.js
@@ -0,0 +1,9 @@
+let copiedHex = ko.observable();
+fetch("./data/vision.json")
+  .then((resp) => resp.json())
+  .then((data) => {
+    console.log(data);
+    ko.applyBindings(data);
+  });
+
+var selectedColor = ko.observable("red"); // lazy
