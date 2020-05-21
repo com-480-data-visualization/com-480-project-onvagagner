caed2b44507c687a81dd89565de97c9360c00b15
Anton <anton.ragot@epfl.ch>
2020-05-22 00:51:58 +0200

    Fixing wine label boxes

--- website/js/label.js
+++ website/js/label.js
@@ -1,39 +1,52 @@
 const label_basic = d3.select("#wine_label")//.attr("viewBox", "0 0 500 650");
-const label_basic_explication = d3.select("#wine_label_explication");
-const label_explanation_text = label_basic_explication.append("text").attr("x", 0).attr("y", "20%").attr("font-size", "20px").attr("text-align", "center")
+const label_basic_explication_title = d3.select("#wine_label_explication_title");
+const label_basic_explication_body = d3.select("#wine_label_explication_body");
+const label_explanation_text_title = label_basic_explication_title.append("text").attr("x", "45%").attr("y", "20%").attr("class", "label_title").attr("font-size", "30px")
+const label_explanation_text_body = label_basic_explication_body.append("text").attr("x", 0).attr("y", "20%").attr("class", "label_body").attr("font-size", "20px")
 
-milesime = {x: 215, y: 236.5, h: 33, w: 70};
-picture = {x: 120, y: 57.85, h: 168, w: 255};
-chateau = {x: 82.5, y: 6.42, h: 45, w: 320};
-origin = {x: 195, y: 300, h: 25.5, w: 110};
-sepage = {x: 205, y: 335, h: 20, w: 90};
-bottled = {x: 109, y: 365, h: 48, w: 285};
-alcool = {x: 10, y: 425, h: 25, w: 110};
-size_liquid = {x: 430, y: 425, h: 25, w: 60};
+chateau = {x: 115, y: 7, h: 45, w: 355};
+picture = {x: 172, y: 66, h: 180, w: 258};
+milesime = {x: 265, y: 265, h: 33, w: 70};
+origin = {x: 240, y: 332, h: 23.5, w: 120};
+sepage = {x: 250, y: 375, h: 20, w: 100};
+bottled = {x: 145, y: 412, h: 48, w: 310};
+alcool = {x: 45, y: 475, h: 25, w: 115};
+size_liquid = {x: 500, y: 475, h: 25, w: 60};
 
 
-function add_element(el, text_to_add) {
+function add_element(el, title, body) {
+  const midLine = 830 - (el.x + el.w);
   const line = label_basic.append('line').attr("class", "label_hoover_line")
-            .attr("x1", el.x + (el.w / 2)).attr("y1", el.y + el.h).attr("x2", el.x + (el.w / 2)).attr("y2", 470);
+            .attr("x1", el.x + el.w).attr("y1", el.y + (el.h / 2)).attr("x2", el.x + el.w + midLine/4).attr("y2", el.y + (el.h / 2));
+  const line2 = label_basic.append('line').attr("class", "label_hoover_line")
+            .attr("x1", el.x + el.w + midLine/4).attr("y1", el.y + (el.h / 2)).attr("x2", el.x + el.w + midLine/4).attr("y2", 20);
+  const line3 = label_basic.append('line').attr("class", "label_hoover_line")
+            .attr("x1", el.x + el.w + midLine/4).attr("y1", 20).attr("x2", 850).attr("y2", 20);
 
   label_basic.append("rect").attr("class", "label_hoover")
             .attr("x", el.x).attr("y", el.y)
             .attr("height", el.h).attr("width", el.w)
             .on("mouseover", () => {
               line.style("stroke-opacity", 1);
-              label_explanation_text.text(text_to_add);
+              line2.style("stroke-opacity", 1);
+              line3.style("stroke-opacity", 1);
+              label_explanation_text_title.text(title);
+              label_explanation_text_body.text(body);
             })
             .on("mouseleave", () => {
               line.style("stroke-opacity", 0);
-              label_explanation_text.text("");
+              line2.style("stroke-opacity", 0);
+              line3.style("stroke-opacity", 0);
+              label_explanation_text_title.text("");
+              label_explanation_text_body.text("");
             });
 }
 
-add_element(milesime, "Milesime or age of the wine");
-add_element(picture, "Graphical representation of the property or logo");
-add_element(chateau, "Chateau");
-add_element(origin, "Region of production")
-add_element(sepage, "Sepage")
-add_element(bottled, "...")
-add_element(alcool, "alcool")
-add_element(size_liquid, "liquid")
+add_element(milesime, "Milesime", "Milesime or age of the wine");
+add_element(picture, "Logo", "Graphical representation of the property or logo");
+add_element(chateau, "Chateau", "11");
+add_element(origin, "Region of production", "qwd")
+add_element(sepage, "Sepage", "wdq")
+add_element(bottled, "...", "cw")
+add_element(alcool, "alcool", "tize")
+add_element(size_liquid, "liquid", "")
